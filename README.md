d7b4a234b6a39b9d276868d92a0bd34bdff50aa0
Baichuan <862843076@qq.com>
2025-06-27 11:51:29 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
-# BEFT
+# BitFit [(Paper)](https://arxiv.org/abs/2106.10199)
+Simple Parameter-efficient Fine-tuning for Transformer-based Masked Language-models
+
+# Abstract
+We introduce BitFit, a sparse-finetuning method where only the bias-terms of the model (or a subset of them) are being modified. We show that with small-to-medium training data, applying BitFit on pre-trained BERT models is competitive with (and sometimes better than) fine-tuning the entire model. For larger data, the method is competitive with other sparse fine-tuning methods.
+Besides their practical utility, these findings are relevant for the question of understanding the commonly-used process of finetuning: they support the hypothesis that finetuning is mainly about exposing knowledge induced by language-modeling training, rather than learning new task-specific linguistic knowledge. 
+
+# Environment 
+First, create an environment with all the dependencies:
+```
+$ conda env create -n bitfit_env -f environment.yml
+```
+Then activate it:
+```
+$ conda activate bitfit_env
+```
+
+# [GLUE Benchmark](https://arxiv.org/abs/1804.07461) evaluation examples:
+
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name <task_name>\
+       --model-name <model_name>\
+       --fine-tune-type <fine_tune_type>\
+       --bias-terms <bias_terms>\
+       --gpu-device <gpu_device>\
+       --learning-rate <learning_rate>\
+       --epochs <epochs>\
+       --batch-size <batch_size>\
+       --optimizer <optimizer_name>\
+       --save-evaluator\
+       --predict-test\
+       --verbose
+```
+For further information about the arguments run:
+```
+python run_glue.py -h
+```
+
+Example of executing full fine tuning:
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\  
+       --model-name bert-base-cased\
+       --fine-tune-type full_ft\
+       --learning-rate 1e-5
+```
+
+Example of executing full BitFit (training all bias terms):
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\
+       --model-name bert-base-cased\
+       --fine-tune-type bitfit\
+       --learning-rate 1e-3
+```
+
+Example of executing partial BitFit (training a subset of the bias terms):
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\
+       --model-name bert-base-cased\
+       --fine-tune-type bitfit\
+       --bias-terms query intermediate\ 
+       --learning-rate 1e-3
+```
+
+Example of executing "frozen" training (i.e. using the pre-trained transformer as a feature extractor):
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\
+       --model-name bert-base-cased\
+       --fine-tune-type frozen\
+       --learning-rate 1e-3
+```
+
+Example of training uniformly chosen trainable parameters (similar to "rand_100k" row in Table 3 in BitFit paper)
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\
+       --model-name bert-base-cased\
+       --fine-tune-type rand_uniform\
+       --learning-rate 1e-3
+```
+
+<!-- Example of training uniformly chosen rows/cols from weight matrices (similar to "rand_row_col" row in Table 3 in BitFit paper)
+```
+python run_glue.py 
+       --output-path <output_path>\
+       --task-name rte\
+       --model-name bert-base-cased\
+       --fine-tune-type rand_uniform\
+       --learning-rate 1e-3
+``` -->
+
+# MIT License
+
+Copyright (c) 2022 benzakenelad
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ run_glue.py
@@ -0,0 +1,236 @@
+"""This file contains a tool that wraps the GLUEvaluator API, the tool supports all the evaluations that were
+performed in BitFit paper (https://arxiv.org/abs/1804.07461), such as: 'full_ft', 'bitfit', 'frozen', 'rand_uniform'
+and 'rand_row_col'.
+
+For questions please reach: benzakenelad@gmail.com
+
+Author Elad Ben-Zaken
+"""
+
+import argparse
+import os
+import logging
+
+from utils import setup_logging
+from glue_evaluator import GLUEvaluator, set_seed
+
+setup_logging()
+LOGGER = logging.getLogger(__file__)
+
+PADDING = "max_length"
+MAX_SEQUENCE_LEN = 128
+
+RAND_UNIFORM_MASK_SIZE = {'bert-base-cased': 100000, 'bert-large-cased': 280000, 'roberta-base': 105000}
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='BitFit GLUE evaluation',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--output-path', '-o', required=False, type=str, default='./results',
+                        help='output directory path for evaluation products.')
+    parser.add_argument('--task-name', '-t', required=False, type=str, default='sst2', help='GLUE task name for evaluation.',
+                        choices={'cola', 'mnli', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli','cb','wic'})
+    parser.add_argument('--model-name', '-m', type=str, default='bert-base-cased', help='model-name to evaluate with.',
+                        choices={'bert-base-cased', 'bert-large-cased', 'roberta-base'})
+
+    parser.add_argument('--training-data-number', type=str, default='gradual', help='how many training data used',
+                        choices={'all', 'gradual', 'other'})
+
+    parser.add_argument('--bias-terms-loop', type=bool, default=True, help='whether bias terms loop')
+                        
+    parser.add_argument('--fine-tune-type', '-f', required=False, type=str, default='bitfit',
+                        help='Which fine tuning process to perform, types are the types that were performed in BitFit paper.',
+                        choices={'full_ft', 'bitfit', 'frozen', 'rand_uniform', 'rand_row_col'})
+    parser.add_argument('--bias-terms', metavar='N', type=str, nargs='+', default=['all'],
+                        choices={'intermediate', 'key', 'query', 'value', 'output', 'output_layernorm',
+                                 'attention_layernorm', 'all','gLN1','gLN2'},
+                        help='bias terms to BitFit, should be given in case --fine-tune-type is bitfit '
+                             '(choose \'all\' for BitFit all bias terms)')
+
+    parser.add_argument('--gpu-device', '-d', type=int, default=0,
+                        help='GPU id for BitFit, if not mentioned will train on CPU.')
+    parser.add_argument('--seed', '-s', type=int, default=0, help='seed value to set.')
+    parser.add_argument('--learning-rate', '-l', type=float, help='learning rate for training.')
+    parser.add_argument('--epochs', '-e', type=int, default=16, help='number of training epochs.')
+    parser.add_argument('--batch-size', '-b', type=int, default=16, help='training and evaluation batch size.')
+    parser.add_argument('--optimizer', type=str, default='adamw', choices={'adam', 'adamw'})
+    parser.add_argument('--save-evaluator', action='store_true', default=False,
+                        help='if given, will save the evaluator for later inference/examination.')
+    parser.add_argument('--predict-test', action='store_true', default=False,
+                        help='if given, will infer on test set using the fine-tuned model (predictions file will be in '
+                             'GLUE benchmark test server format). Predictions will be saved to output_path.')
+    parser.add_argument('--verbose', action='store_true', default=True,
+                        help='if given, will plot a list of trainable weights.')
+
+    return parser.parse_args()
+
+
+def _validate_args(args):
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+    if not os.path.isdir(args.output_path):
+        raise ValueError("--output_path must be a path to directory")
+    # if len(os.listdir(args.output_path)):
+    #     raise ValueError("--output_path directory isn't empty, please supply an empty directory path.")
+    if args.fine_tune_type == 'rand_uniform' and args.model_name not in RAND_UNIFORM_MASK_SIZE.keys():
+        raise ValueError(f'Currently the rand_uniform fine-tune type is not supported for {args.model_name}.')
+
+def _plot_training_details(args,data_size):
+    [LOGGER.info('############################################################################################') for _
+     in range(3)]
+    LOGGER.info('')
+
+    LOGGER.info('Training Details: ')
+    LOGGER.info('----------------------------------------------')
+    LOGGER.info(f'Model Name: {args.model_name}')
+    LOGGER.info(f'Task Name: {args.task_name}')
+    LOGGER.info(f'Fine Tuning Type: {args.fine_tune_type}')
+    LOGGER.info(f'Output Directory: {args.output_path}')
+
+    if args.gpu_device is not None:
+        LOGGER.info(f'Running on GPU #{args.gpu_device}')
+    else:
+        LOGGER.info(f'Running on CPU')
+
+    LOGGER.info(f'Training Data Number: {args.training_data_number}')
+
+    if args.fine_tune_type == 'bitfit':
+        LOGGER.info(f"Bias Trainable Terms: {'all bias terms' if 'all' in args.bias_terms else args.bias_terms}")
+
+
+    lr_bert_base = {'cola':7e-4, 'mnli':1e-4, 'mrpc':7e-4, 'qnli':1e-4, 'qqp':4e-4, 'rte':1e-3, 'sst2':4e-4, 'stsb':1e-4,'cb':1e-3,'wic':1e-3}
+
+    LOGGER.info(f'Epochs: {args.epochs}')
+    if args.fine_tune_type == 'bitfit':
+        args.learning_rate = lr_bert_base[args.task_name]
+    elif args.fine_tune_type == 'full_ft':
+        args.learning_rate = 1e-5
+
+    if data_size =='all':
+        LOGGER.info('training data size: all')
+    else:
+        LOGGER.info(f'training data size: {data_size}')
+    LOGGER.info(f'Learning Rate: {args.learning_rate}')
+    LOGGER.info(f'Batch Size: {args.batch_size}')
+    LOGGER.info(f"Optimizer: {'AdamW' if args.optimizer == 'adamw' else 'Adam'}")
+
+    LOGGER.info('')
+    [LOGGER.info('############################################################################################') for _
+     in range(3)]
+
+
+def _perform_training_preparations(evaluator, args, trainable_components):
+    if args.fine_tune_type == 'frozen':
+        trainable_components = []
+
+    if args.fine_tune_type == 'full_ft':
+        evaluator.training_preparation(learning_rate=args.learning_rate,
+                                       optimizer=args.optimizer,
+                                       encoder_trainable=True,
+                                       verbose=args.verbose)
+    elif args.fine_tune_type in {'bitfit', 'frozen'}:
+        evaluator.training_preparation(learning_rate=args.learning_rate,
+                                       optimizer=args.optimizer,
+                                       encoder_trainable=False,
+                                       trainable_components=trainable_components,
+                                       verbose=args.verbose)
+    else:
+        evaluator.training_preparation(learning_rate=args.learning_rate,
+                                       optimizer=args.optimizer,
+                                       encoder_trainable=True,
+                                       verbose=False)
+
+        # randomizing mask
+        if args.fine_tune_type == 'rand_uniform':
+            evaluator.set_uniform_mask(mask_size=RAND_UNIFORM_MASK_SIZE[args.model_name])
+        else:  # args.fine_tune_type == 'rand_row_col'
+            evaluator.set_row_and_column_random_mask()
+
+
+def main(args):
+    # args parsing
+    _validate_args(args)
+    #_plot_training_details(args)
+
+
+    if args.training_data_number == 'all':
+        _plot_training_details(args,'all')
+        # seed
+        set_seed(args.seed)
+
+        evaluator = GLUEvaluator(args.task_name, args.model_name, args.gpu_device,args.training_data_number,args.fine_tune_type,args.bias_terms)
+
+        evaluator.preprocess_dataset(PADDING, MAX_SEQUENCE_LEN, args.batch_size)
+
+        # training preparation
+        trainable_components = GLUEvaluator.convert_to_actual_components(args.bias_terms)
+        _perform_training_preparations(evaluator, args, trainable_components)
+
+        # train and evaluate
+        evaluator.train_and_evaluate(args.epochs, args.output_path,args.epochs-1) # final evaluation
+
+        # saving artifacts
+        # #if args.fine_tune_type == 'bitfit':
+        if not args.bias_terms_loop:
+            evaluator.plot_terms_changes(os.path.join(args.output_path, 'bias_term_changes_'+args.fine_tune_type+'_'+args.task_name+'_'+'alldata'))
+            evaluator.plot_terms_angles(os.path.join(args.output_path, 'angles_term_changes_'+args.fine_tune_type+'_'+args.task_name+'_'+'alldata'))
+
+        # save model
+        if args.save_evaluator:
+            evaluator.save(os.path.join(args.output_path, 'evaluator'))
+
+        # export model test set predictions
+        if args.predict_test:
+            evaluator.export_model_test_set_predictions(args.output_path)
+
+    elif args.training_data_number == 'gradual':
+        train_size_loop = [1000] # [500,1000,3000,5000,7000,9000,11000,] # sst2
+        #train_size_loop = [1000,3000,5000,7000,9000] #mnli qqp qnli
+        #train_size_loop =[500] # [100,200,300,500,700,900,1200,2490] RTE
+        #train_size_loop = [0.05,0.1,0.2,0.3,0.4,0.5,1] # Cola mrpc stsb
+        # data preprocessing
+        
+        for train_size in train_size_loop:
+            _plot_training_details(args,train_size)
+            # seed
+            set_seed(args.seed)
+
+            # evaluator creation
+            evaluator = GLUEvaluator(args.task_name, args.model_name, args.gpu_device,str(int(train_size)),args.fine_tune_type,args.bias_terms)
+
+            evaluator.preprocess_dataset(PADDING, MAX_SEQUENCE_LEN, args.batch_size,train_size=train_size)
+
+            # training preparation
+            trainable_components = GLUEvaluator.convert_to_actual_components(args.bias_terms)
+            _perform_training_preparations(evaluator, args, trainable_components)
+
+            # train and evaluate
+            evaluator.train_and_evaluate(args.epochs, args.output_path,args.epochs-1)
+
+            # saving artifacts
+            # #if args.fine_tune_type == 'bitfit':
+            if not args.bias_terms_loop:
+                evaluator.plot_terms_changes(os.path.join(args.output_path, 'bias_term_changes_'+args.fine_tune_type+'_'+args.task_name+'_'+str(int(train_size))))
+                evaluator.plot_terms_angles(os.path.join(args.output_path, 'angles_term_changes_'+args.fine_tune_type+'_'+args.task_name+'_'+str(int(train_size))))
+
+            # save model
+            if args.save_evaluator:
+                evaluator.save(os.path.join(args.output_path, 'evaluator'))
+
+            # export model test set predictions
+            if args.predict_test:
+                evaluator.export_model_test_set_predictions(args.output_path)
+
+if __name__ == '__main__':
+
+    args = _parse_args()
+    bias_terms_loop = ['value','query','key'] # ,'intermediate','m1', 'm3','LN1','LN2']
+    #bias_terms_loop = ['m3','LN2']
+    if args.bias_terms_loop:
+        for bias_terms in bias_terms_loop:
+            args.bias_terms = [bias_terms]
+            main(args)
+    elif not args.bias_terms_loop:
+        main(args)
+
